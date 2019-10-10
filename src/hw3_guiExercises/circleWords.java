7a80c696af6108f48058bb7d8a446375b83f493b
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-10-09 18:43:03 -0600

    circle words works and hangman has a dead face

--- src/hw3_guiExercises/circleWords.java
+++ src/hw3_guiExercises/circleWords.java
@@ -3,9 +3,7 @@ package hw3_guiExercises;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -14,19 +12,26 @@ public class circleWords extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        Pane pane = new StackPane();
-        Circle circle = new Circle(100);
+        Pane pane = new Pane();
+        int[] CENTER = {200,200};
+        String message = "Lets get this bread fam  ";
+        String style = "-fx-font: 38 rockwell;";
+        double RADIUS = 75.0;
 
-        Text message =  new Text("Welcome to JavaFX");
-        message.setFill(Color.WHITE);
+        for (int i  = 0; i < message.length(); i++) {
+            double arcAngle = (i * (2*Math.PI) / message.length()) + Math.PI;
+            Text t = new Text(String.valueOf(message.charAt(i)));
+            t.setStyle(style);
+            t.setX(CENTER[0] + Math.cos(arcAngle) * RADIUS);
+            t.setY(CENTER[1] + Math.sin(arcAngle) * RADIUS);
+            t.setRotate(90 + arcAngle*180/Math.PI);
+            pane.getChildren().add(t);
+        }
 
-        pane.getChildren().add(circle);
-        pane.getChildren().add(message);
 
-
-        Scene s = new Scene(pane);
+        Scene s = new Scene(pane, 400, 400);
         primaryStage.setScene(s);
-        primaryStage.setTitle("HangMan Drawing");
+        primaryStage.setTitle("Circle Words");
         primaryStage.show();
     }
 }
