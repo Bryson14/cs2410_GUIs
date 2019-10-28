e0f06c3bcaa70491983537d0c8f1ef4f5d79d855
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-10-28 14:16:25 -0600

    wow worst chatroom ever haha

--- src/inClass/chatroom/ChatBotClient.java
+++ src/inClass/chatroom/ChatBotClient.java
@@ -1,4 +1,35 @@
 package inClass.chatroom;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ChatBotClient {
+    public static void main(String[] args) throws Exception {
+        BufferedWriter wr;
+        BufferedReader rd;
+        Scanner scan;
+        Socket sck;
+        String response, send;
+
+        sck = new Socket("144.39.205.68", 5555);
+        rd = new BufferedReader(new InputStreamReader(sck.getInputStream()));
+        wr = new BufferedWriter(new OutputStreamWriter(sck.getOutputStream()));
+        scan = new Scanner(System.in);
+
+        while(true) {
+            System.out.print("> ");
+            send = scan.nextLine();
+            wr.write(send + "\r\n");
+            wr.flush();
+            response = rd.readLine().trim();
+            System.out.println("From Server: " + response);
+
+        }
+
+    }
 }
