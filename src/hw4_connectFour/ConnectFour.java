90da1ed33fc235717e2144b500bf52831978f4ff
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-10-30 17:56:03 -0600

    idk why stuff doesn't work

--- src/hw4_connectFour/ConnectFour.java
+++ src/hw4_connectFour/ConnectFour.java
@@ -1,10 +1,13 @@
 package hw4_connectFour;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -12,12 +15,18 @@ public class ConnectFour extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Pane pane = new HBox();
-        ConnectFourPane c4 = new ConnectFourPane();
-        c4.newGame();
-        pane.getChildren().add(new Text("Hello"));
+        Pane basePane = new StackPane();
+        basePane.getChildren().add(new Circle(100, Color.GRAY ));
+        testScene test = new testScene();
 
-        primaryStage.setScene(c4.getScene());
+
+
+
+        Text message = new Text("Welcome to Connect Four");
+        VBox Vpane = new VBox(basePane, message);
+        Vpane.setAlignment(Pos.BOTTOM_CENTER);
+//        primaryStage.setScene(new Scene(Vpane));
+        primaryStage.setScene(test.getScene());
         primaryStage.show();
     }
 }
